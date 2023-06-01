d3f7144f9bc4c9a5f3c1d56441bfa22286078a96
Taylor Watters <taylorbbigelow@gmail.com>
2023-06-01 15:52:06 -0700

    adds achievement model based on ERD

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
 const { userRouter } = require('./user.js');
+const { achievementRouter } = require('./achievement.js');
+
 const apiRouter = express.Router();
 
 apiRouter.use('/api/user', userRouter);
+apiRouter.use('/api/achievement', achievementRouter);
 
 module.exports = { apiRouter };
