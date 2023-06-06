deb6b9cac90968437b185455ce67e22fc98e609e
Taylor Watters <taylorbbigelow@gmail.com>
2023-06-05 19:47:02 -0700

    adds login/signup routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,22 +1,44 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const { User } = require('../models');
 
 const userRouter = express.Router();
 
 userRouter.get('/', async (req, res) => {
     const allUsers = await User.findAll({});
-    return res.json(allUsers);
+    return res.status(200).json(allUsers);
 });
 
-userRouter.post('/', async (req, res) => {
+userRouter.post('/signup', async (req, res) => {
     try {
         const { username, password, firstName, lastName } = req.body;
         const user = await User.create({ username, password, firstName, lastName });
 
-        res.json({ user });
+        res.status(200).json({ user });
     } catch (error) {
-        res.json({ error });
+        res.status(500).json({ error });
     }
+});
+
+userRouter.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ where: { username } });
+  const isValid = bcrypt.compareSync(password, user.password);
+
+  if (isValid) {
+    const token = jwt.sign(
+      { username },
+      process.env.TOKEN_SECRET,
+      { expiresIn: '1h' },
+    );
+  
+    res.status(200).json({ token });
+  } else {
+    res.status(401);
+  }
 });
 
 module.exports = { userRouter };
