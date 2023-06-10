5a41f818d008ddd65805135acfa3f014fa43988a
Taylor Watters <taylorbbigelow@gmail.com>
2023-06-09 19:03:07 -0700

    cors config pt2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -8,15 +9,9 @@ const { apiRouter } = require('./controllers');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(apiRouter);
 
-app.use((_, res, next) => {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
 
 sequelize.sync().then(() => {
     app.listen(PORT, () => {
