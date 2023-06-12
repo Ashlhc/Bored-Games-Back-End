1a0699272a9c2696400151c23b50c2812886695d
Taylor Watters <taylorbbigelow@gmail.com>
2023-06-11 20:58:24 -0700

    stop testing in prod

--- controllers/user.js
+++ controllers/user.js
@@ -17,13 +17,19 @@ userRouter.get('/', async (_, res) => {
     }
 });
 
-userRouter.get('/:username', async (_, res) => {
+userRouter.get('/:username', async (req, res) => {
     try {
       const user = await User.findOne({
-        where: { username },
+        where: { username: req.params.username },
       });
-      return res.status(200).json(user);
+
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: 'User not found' });
+      }
     } catch (error) {
+      console.log(error);
       res.status(500).json({ error });
     }
 });
