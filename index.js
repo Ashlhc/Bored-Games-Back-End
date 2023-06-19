af4763bafa29ec2c1d194761d72d00b3a804787a
Taylor Watters <taylorbbigelow@gmail.com>
2023-06-18 21:00:35 -0700

    fixes cors

--- index.js
+++ index.js
@@ -44,7 +44,9 @@ app.use((req, res, next) => {
   if (isValidOrigin(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
