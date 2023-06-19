0820f06f55ca7c0d4f1679cceebff9a0d0cb1b48
Taylor Watters <taylorbbigelow@gmail.com>
2023-06-18 20:53:06 -0700

    adding another preflight header

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const io = new Server(server, {
             callback(new Error('Not allowed by CORS'));
           }
         },
-        methods: ['GET', 'POST', 'DELETE'],
+        methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
     },
 });
 
